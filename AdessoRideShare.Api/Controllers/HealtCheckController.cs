0ebb81d814dde3a44e644d85b64a790d71008e15
Dogukan TUNCBILEK <dogutuncbilek35@gmail.com>
2023-01-12 22:29:01 +0300

    start page changed

---
+++ AdessoRideShare.Api/Controllers/HealtCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdessoRideShare.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealtCheckController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult HealtCheck()
+        {
+            return Ok(new { ping = "pong", date = DateTime.Now });
+        }
+    }
+}
